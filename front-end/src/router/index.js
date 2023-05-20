a018a6ba31a94949dc21f460f82f6462ca031aaa
Luiz Fernando <fernandoluiz43673@gmail.com>
2023-05-20 00:11:55 -0300

    added redirect to home when receive 404 error

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -15,6 +15,11 @@ const router = createRouter({
       name: "tour",
       component: TourView,
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: "not-found",
+      component: HomeView,
+    },
   ],
 });
 
